fe1f553aeb30c765c7f7a7c25a33b65e422eb7de
kumandev <kumandev2016@gmail.com>
2021-02-16 06:36:29 +0800

    Just update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # biznet-mrtg-average-report
+
+This script perform data extraction from Biznet MRTG only for average upload and download traffic.<br>
+The code may full of mess, but it helped me a lot.
+
+<b>Requirement:</b><br>
+- Python3 <br>
+- Firefox webdriver<br>
+- Tesseract OCR, set the correct path to the tesseract.exe. Find the installer inside the ocr folder<br>
+https://medium.com/quantrium-tech/installing-and-using-tesseract-4-on-windows-10-4f7930313f82
+
+<b>Usage:</b><br>
+$python mrtg.py yyyy-mm-dd<br>
+
+Result will be stored in the log folder as txt file
--- mrtg.py
+++ mrtg.py
@@ -1,28 +1,38 @@
+#!/usr/bin/env python
+
 from selenium import webdriver
 import time, sys, os
 from PIL import Image
 from pytesseract import pytesseract 
 
-#set clock, test
-clock = ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+#mrtg credentials
+mrtg_username = ""
+mrtg_password = ""
+
+#set target clock here
+clock = ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
 target_date = sys.argv[1]
 
+#output folder 
 path_ss = "screenshot/"+target_date+"/"
-path_croped = "croped/"+target_date+"/"
+path_cropped = "cropped/"+target_date+"/"
 path_log = "log/"
 
+# create folder and log file 
 os.system("mkdir "+path_ss.replace("/","\\"))
-os.system("mkdir "+path_croped.replace("/","\\"))
-
+os.system("mkdir "+path_cropped.replace("/","\\"))
 log_name = "log-average-"+target_date+".txt"
 
 # initiate web driver
-driver = webdriver.Firefox(executable_path=r'C:\webdriver\geckodriver.exe')
+driver = webdriver.Firefox(executable_path=r'webdriver\geckodriver.exe')
+driver.set_window_position(0, 0)
+driver.set_window_size(1366, 768)
+
 #login
 driver.get("http://mrtg-branch.biznetnetworks.com/cacti/graph_view.php")
 time.sleep(0.5)
-driver.find_element_by_xpath("/html/body/form/table/tbody/tr[5]/td[2]/input").send_keys("username")
-driver.find_element_by_xpath("/html/body/form/table/tbody/tr[6]/td[2]/input").send_keys("password")
+driver.find_element_by_xpath("/html/body/form/table/tbody/tr[5]/td[2]/input").send_keys(mrtg_username)   
+driver.find_element_by_xpath("/html/body/form/table/tbody/tr[6]/td[2]/input").send_keys(mrtg_password) 
 driver.find_element_by_xpath("/html/body/form/table/tbody/tr[8]/td/input").click()
 
 
@@ -58,14 +68,14 @@ def download_graph(i, start_date, end_date):
 
 def extract_text(i):
 	img_ss = path_ss+target_date+" "+clock[i+1][:2]+".png"
-	img_crop = path_croped+"crop-"+target_date+clock[i+1][:2]+".png"
+	img_crop = path_cropped+"crop-"+target_date+clock[i+1][:2]+".png"
 
 	img = Image.open(img_ss)
 	# img_crop_data = img.crop((677, 525, 758, 578))
-	img_crop_data = img.crop((720, 403, 793, 450))
+	img_crop_data = img.crop((720, 403, 793, 450))   #adjust this line to get correct cropped area for average value
 	img_crop_data.save(img_crop, quality=100)
 
-	print("["+str(i+1)+"] "+"Extracting data from : "+path_croped+img_ss+" to "+log_name)
+	print("["+str(i+1)+"] "+"Extracting data from : "+path_cropped+img_ss+" to "+log_name)
 	pytesseract.tesseract_cmd = r'C:\tesseract-ocr\tesseract.exe'
 	text = pytesseract.image_to_string(img_crop)
 	text = text.replace(chr(10), ";").replace(".",",")
@@ -76,15 +86,13 @@ def extract_text(i):
 	text_file.close()
 
 def main():
-	counter = 0
-	
-	for i in range(0,15):
-		if counter < 16:
-			download_graph(i,target_date+clock[i],target_date+clock[i+1])
-			counter+=1
-			if counter == 15:
-				for x in range(0,15):
-					extract_text(x)
+	length = len(clock)-1
+
+	for i in range(0, length): #perform loop as many as the target-clock
+		print("Sequence : "+str(i+1)+", from length : "+str(length))
+		if i < length: 
+			download_graph(i, target_date+" "+clock[i], target_date+" "+clock[i+1])  #(index, start_date, end_date)
+			extract_text(i) #i refer to the index of "clock" variable
 		else:
 			print("Do nothing...")
 
