9ba2243b9c2cd0ead161c03caeb1bc4461db70ab
kumandev <kumandev2016@gmail.com>
2021-02-25 00:18:11 +0800

    Add function driver_init()

--- mrtg.py
+++ mrtg.py
@@ -9,6 +9,8 @@ from pytesseract import pytesseract
 mrtg_username = ""
 mrtg_password = ""
 
+url_login = "http://mrtg-branch.biznetnetworks.com/cacti/graph_view.php"
+
 #set target clock here
 clock = ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
 target_date = sys.argv[1]
@@ -23,17 +25,18 @@ os.system("mkdir "+path_ss.replace("/","\\"))
 os.system("mkdir "+path_cropped.replace("/","\\"))
 log_name = "log-average-"+target_date+".txt"
 
-# initiate web driver
+# create web driver object
 driver = webdriver.Firefox(executable_path=r'webdriver\geckodriver.exe')
-driver.set_window_position(0, 0)
-driver.set_window_size(1366, 768)
 
-#login
-driver.get("http://mrtg-branch.biznetnetworks.com/cacti/graph_view.php")
-time.sleep(0.5)
-driver.find_element_by_xpath("/html/body/form/table/tbody/tr[5]/td[2]/input").send_keys(mrtg_username)   
-driver.find_element_by_xpath("/html/body/form/table/tbody/tr[6]/td[2]/input").send_keys(mrtg_password) 
-driver.find_element_by_xpath("/html/body/form/table/tbody/tr[8]/td/input").click()
+def driver_init():
+	driver.set_window_position(0, 0)
+	driver.set_window_size(1366, 768)
+	#login
+	driver.get(url_login)
+	time.sleep(0.5)
+	driver.find_element_by_xpath("/html/body/form/table/tbody/tr[5]/td[2]/input").send_keys(mrtg_username)   
+	driver.find_element_by_xpath("/html/body/form/table/tbody/tr[6]/td[2]/input").send_keys(mrtg_password) 
+	driver.find_element_by_xpath("/html/body/form/table/tbody/tr[8]/td/input").click()
 
 
 def download_graph(i, start_date, end_date):
@@ -72,7 +75,7 @@ def extract_text(i):
 
 	img = Image.open(img_ss)
 	# img_crop_data = img.crop((677, 525, 758, 578))
-	img_crop_data = img.crop((720, 403, 793, 450))   #adjust this line to get correct cropped area for average value
+	img_crop_data = img.crop((720, 420, 790, 469))   #adjust this line to get correct cropped area for average value
 	img_crop_data.save(img_crop, quality=100)
 
 	print("["+str(i+1)+"] "+"Extracting data from : "+path_cropped+img_ss+" to "+log_name)
@@ -86,6 +89,8 @@ def extract_text(i):
 	text_file.close()
 
 def main():
+	driver_init()
+
 	length = len(clock)-1
 
 	for i in range(0, length): #perform loop as many as the target-clock
@@ -93,8 +98,13 @@ def main():
 		if i < length: 
 			download_graph(i, target_date+" "+clock[i], target_date+" "+clock[i+1])  #(index, start_date, end_date)
 			extract_text(i) #i refer to the index of "clock" variable
+			if i == length-1:
+				text_file = open(path_log+log_name, "a+")
+				text_file.write("=====\n")
+				text_file.close()
 		else:
 			print("Do nothing...")
 
 if __name__ == '__main__':
 	main()
+	driver.quit()
